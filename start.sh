4d3d17fe2deb1647ce9535414062d4d9e8f4eaae
Andrei Kuchuk <42741840+Sabrjkee@users.noreply.github.com>
2019-09-23 09:10:34 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo apt-get install  curl  apt-transport-https ca-certificates software-properties-common mc
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose up -d
