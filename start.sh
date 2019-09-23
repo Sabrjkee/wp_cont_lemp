6c19b1564d7f1177730a4e31a36c611be3dc416b
Andrei Kuchuk <42741840+Sabrjkee@users.noreply.github.com>
2019-09-23 09:14:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,6 @@ sudo apt-get install docker-ce
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+sudo usermod -aG docker $USER
+
 docker-compose up -d
